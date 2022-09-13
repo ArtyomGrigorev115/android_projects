26f00fb4ae8d0975da8433fc6a7fcf8ff20cb453
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-13 22:24:45 +0700

    создал модуль color descriptor

---
+++ color_descriptor/src/main/java/com/example/color_descriptor/ColorDescriptorMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.color_descriptor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ColorDescriptorMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_color_descriptor_main);
+    }
+}
