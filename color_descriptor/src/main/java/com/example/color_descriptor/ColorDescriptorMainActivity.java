3c3681093000761004bbd6ba8805773e67a46fe4
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-13 23:03:13 +0700

    сделал макет приложения

--- color_descriptor/src/main/java/com/example/color_descriptor/ColorDescriptorMainActivity.java
+++ color_descriptor/src/main/java/com/example/color_descriptor/ColorDescriptorMainActivity.java
@@ -10,5 +10,7 @@ public class ColorDescriptorMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color_descriptor_main);
+
+
     }
 }
