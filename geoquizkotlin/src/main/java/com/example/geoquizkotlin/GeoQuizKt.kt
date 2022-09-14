885cce9ca302839fe8d519e6ce8ae00f4beda1dd
ArtyomGrigorev115 <artyomgrigorev115@gmail.com>
2022-09-14 16:50:00 +0700

    init: создал проект GeoQiz на Kotlin

---
+++ geoquizkotlin/src/main/java/com/example/geoquizkotlin/GeoQuizKt.kt
@@ -0,0 +1,11 @@
+package com.example.geoquizkotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GeoQuizKt : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_geo_quiz_kt)
+    }
+}
